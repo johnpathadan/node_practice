f1eb71cbca37f802fd05a2632a866b494f166277
johnpathadan <johnpathadan@gmail.com>
2022-09-20 19:48:30 +0530

    fix: js refresher

---
+++ jsrefresher.js
@@ -0,0 +1,9 @@
+const userName = "Max";
+const age = 29;
+const hasHobbies = true;
+
+const inASentence = (userName, age, hasHobbies) => (
+    console.log(`${userName} is ${age} years old`)
+);
+
+inASentence(userName, age);
